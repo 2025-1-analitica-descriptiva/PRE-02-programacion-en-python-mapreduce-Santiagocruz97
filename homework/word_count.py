9c577e4c66c3b2cf18238b64a1b1fefdcfa85ea2
Santiago C. <scruza@unal.edu.co>
2025-04-25 14:57:27 -0500

    Conteo Palabras

--- homework/word_count.py
+++ homework/word_count.py
@@ -5,8 +5,8 @@
 import fileinput
 import glob
 import os.path
-import time
 import string
+import time
 from itertools import groupby
 
 
@@ -21,17 +21,22 @@ from itertools import groupby
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
 
-    if not os.path.exists("files/input"):
-        os.makedirs("files/input")
+
+    if os.path.exists("files/input"):
+        for file in glob.glob("files/input/*"):
+            os.remove(file)
+        os.rmdir("files/input")
+    os.makedirs("files/input")
 
     for file in glob.glob("files/raw/*"):
         for i in range(1, n + 1):
             with open(file, "r", encoding="utf-8") as f:
-                with open(f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt", "w", encoding="utf-8") as f2:
-                    f2.write(f.read())
-
-
-
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())  
 #
 # Escriba la función load_input que recive como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
@@ -49,12 +54,14 @@ def copy_raw_files_to_input_folder(n):
 #
 def load_input(input_directory):
     """Funcion load_input"""
+
     sequence = []
     files = glob.glob(f"{input_directory}/*")
     with fileinput.input(files=files) as f:
         for line in f:
             sequence.append((fileinput.filename(), line))
     return sequence
+
 
 #
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
@@ -68,6 +75,7 @@ def line_preprocessing(sequence):
         for key, value in sequence
     ]
     return sequence
+  
 
 
 #
@@ -86,7 +94,6 @@ def mapper(sequence):
     """Mapper"""
     return [(word, 1) for _, value in sequence for word in value.split()]
 
-
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
 # por el mapper, y retorna una lista con el mismo contenido ordenado por la
@@ -100,6 +107,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -110,6 +118,10 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    result = []
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
 
 
 #
@@ -118,6 +130,13 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
+
 
 
 #
@@ -130,6 +149,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -138,19 +160,24 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
-
-from pprint import pprint
+#
 def run_job(input_directory, output_directory):
     """Job"""
     sequence = load_input(input_directory)
     sequence = line_preprocessing(sequence)
     sequence = mapper(sequence)
-    pprint(sequence[:5])
-
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+    return
 
 if __name__ == "__main__":
 
